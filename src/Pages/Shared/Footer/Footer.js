053366dfd2a051abaca1816fbdddd412463e5d3b
Your Name <you@example.com>
2022-10-19 07:20:44 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h5>CopyRight By Me</h5>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpM8-7h1Bd3OOyuX9fmaqV8-8AEo6-Y0I",
+  authDomain: "dragone-news-d1536.firebaseapp.com",
+  projectId: "dragone-news-d1536",
+  storageBucket: "dragone-news-d1536.appspot.com",
+  messagingSenderId: "3666868719",
+  appId: "1:3666868719:web:83c3f69f97a8f6f1419e1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
