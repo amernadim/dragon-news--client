17ca05a0192d25ab98dde376c46dbf600ab4236c
Your Name <you@example.com>
2022-10-22 08:39:50 +0600

    deploy

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-  const [categories , setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-      fetch('http://localhost:5000/news-categories')
-      .then(res => res.json())
-      .then(data => setCategories(data))
-  },[])
+    fetch("https://dragon-news-server-mauve.vercel.app/news-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
 
-  return ( 
-    <div className='d-none d-lg-block'>
+  return (
+    <div className="d-none d-lg-block">
       <h5>All Categories : {categories.length}</h5>
       <div>
-        {
-          categories.map(category => <p key={category.id}>
-              <Link to={`/category/${category.id}`}>{category.name}</Link>
-          </p>)
-        }
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
       </div>
     </div>
   );
--- src/routers/Routers/Router.js
+++ src/routers/Routers/Router.js
@@ -10,39 +10,59 @@ import News from "../../Pages/News/News/News";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
-  {path : '/',
-   element : <Main/>,
-   children : [
-    {
-      path: '/', 
-      element : <PrivateRoute><Home></Home></PrivateRoute>,
-      loader : () => fetch('http://localhost:5000/news')
-    },
-    {
-      path: '/category/:id', 
-      element : <Category></Category>,
-      loader : ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
-    },
-    {
-      path: '/news/:id', 
-      element : <PrivateRoute><News></News></PrivateRoute>,
-      loader : ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
-    },
-    {
-      path : '/login' ,
-      element : <Login/>
-    },
-    {
-      path : '/register',
-      element : <Register/>
-    },
-    {
-      path : '/terms',
-      element : <TermsAndCondition/>
-    },
-    {
-      path : '/profile',
-      element : <PrivateRoute><Profile/></PrivateRoute>
-    }
-  ]}
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <PrivateRoute>
+            <Home></Home>
+          </PrivateRoute>
+        ),
+        loader: () => fetch("https://dragon-news-server-mauve.vercel.app/news"),
+      },
+      {
+        path: "/category/:id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(
+            `https://dragon-news-server-mauve.vercel.app/category/${params.id}`
+          ),
+      },
+      {
+        path: "/news/:id",
+        element: (
+          <PrivateRoute>
+            <News></News>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://dragon-news-server-mauve.vercel.app/news/${params.id}`
+          ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/terms",
+        element: <TermsAndCondition />,
+      },
+      {
+        path: "/profile",
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
